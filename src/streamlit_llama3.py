a8b66af33c16cd4fc80d55b291683408560747f6
matthew.lessler@everwatchsolutions.com <ubuntu@ip-172-31-36-85.ec2.internal>
2024-07-17 18:24:03 +0000

    Updated changes

--- src/streamlit_llama3.py
+++ src/streamlit_llama3.py
@@ -1,20 +1,15 @@
 #import Essential dependencies
 import streamlit as st
+import os
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS, Chroma
-from langchain.chains import RetrievalQA
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.llms import Ollama
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-from langchain_community.embeddings import OllamaEmbeddings
-from langchain_community.llms import Ollama
-from langchain.prompts import PromptTemplate
-import os
-import shutil
-from typing import List
 from langchain.schema import Document
-# os.system("ollama pull llama3")
 
 # Specify the correct path to your documents directory
 DATA_PATH = '../data'
@@ -27,9 +22,9 @@ def load_documents():
 
 def split_text(docs, max_length=512, chunk_overlap=50):
     splitter = RecursiveCharacterTextSplitter(
-            chunk_size=max_length,
-            chunk_overlap=chunk_overlap
-        )
+        chunk_size=max_length,
+        chunk_overlap=chunk_overlap
+    )
     chunks = []
     for doc in docs:
         chunks.extend(splitter.split_text(doc.page_content))
@@ -67,41 +62,27 @@ def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 if __name__ == '__main__':
-    # Creates header for streamlit app and writes to it
     st.header("Welcome to the 📝 PDF Bot")
     st.write("🤖 You can chat by entering your queries")
 
-    # Check if vectorstore exists, if not create it
     if not os.path.exists(DB_FAISS_PATH):
         create_knowledgeBase()
-        
-    # Load knowledge base and other components
+
     knowledgeBase = load_knowledgeBase()
     llm = load_llm()
     prompt = load_prompt()
 
-    # Creates text box for user to query data
     query = st.text_input('Enter some text')
 
     if query:
-        # Gets most similar vectors from knowledge base to user query and turns into actual documents
-            similar_embeddings = knowledgeBase.similarity_search(query)
-        
-        # Turn the similar embeddings into documents
-            documents = [Document(page_content=doc.page_content) for doc in similar_embeddings]
-        
-        # Create a retriever
-            retriever = FAISS.from_documents(documents=documents, embedding=OllamaEmbeddings(model="mxbai-embed-large", show_progress=True)).as_retriever()
-        
-        # Define the chain together with query, documents, prompt, and LLM to form process for generating response
-            rag_chain = (
-                {"context": retriever | format_docs, "question": RunnablePassthrough()}
-                | prompt
-                | llm
-                | StrOutputParser()
-            )
-        
-        # Calls chain and writes response to streamlit
-            response = rag_chain.invoke(query)
-            st.write(response)
-
+        similar_embeddings = knowledgeBase.similarity_search(query)
+        documents = [Document(page_content=doc.page_content) for doc in similar_embeddings]
+        retriever = FAISS.from_documents(documents=documents, embedding=OllamaEmbeddings(model="mxbai-embed-large", show_progress=True)).as_retriever()
+        rag_chain = (
+            {"context": retriever | format_docs, "question": RunnablePassthrough()}
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+        response = rag_chain.invoke(query)
+        st.write(response)
