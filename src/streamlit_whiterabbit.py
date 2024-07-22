a51c8dc173dc5ccc0cab2b936e36f3963c29922f
CMullikin-ews <connor.mullikin@everwatchsolutions.com>
2024-07-22 19:23:25 +0000

    E

--- src/streamlit_whiterabbit.py
+++ src/streamlit_whiterabbit.py
@@ -174,9 +174,9 @@ def format_docs(docs):
 if __name__=='__main__':
         # Downloads and runs ollama, as well as pulling our embedding model and LLM
         # os.system("curl -fsSL https://ollama.com/install.sh | sh")
-        from subprocess import Popen
+        os.system("export OLLAMA_HOST=0.0.0.0")
         os.system("sudo service ollama stop")
-        p = Popen([os.system("ollama serve")])
+        os.system("ollama serve")
         os.system("ollama pull mxbai-embed-large")
         
         # Creates header for streamlit app and writes to it
