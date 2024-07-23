d6f584305887c17f4f8ed66a0e27caf956bacbe6
CMullikin-ews <connor.mullikin@everwatchsolutions.com>
2024-07-23 12:53:06 +0000

    f

--- src/streamlit_whiterabbit.py
+++ src/streamlit_whiterabbit.py
@@ -173,9 +173,9 @@ def format_docs(docs):
 
 if __name__=='__main__':
         # Downloads and runs ollama, as well as pulling our embedding model and LLM
-        os.system("curl -fsSL https://ollama.com/install.sh | sh")
-        os.system("export OLLAMA_HOST=localhost:8888")
-        os.system("sudo service ollama stop")
+        # os.system("curl -fsSL https://ollama.com/install.sh | sh")
+        # os.system("export OLLAMA_HOST=localhost:8888")
+        # os.system("sudo service ollama stop")
         os.system("ollama serve")
         os.system("ollama pull mxbai-embed-large")
         os.system("ollama pull jimscard/whiterabbit-neo")
