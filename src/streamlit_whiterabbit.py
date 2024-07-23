a9e6a803e441b5191383fd904ff87455d98dd99e
CMullikin-ews <connor.mullikin@everwatchsolutions.com>
2024-07-23 18:04:00 +0000

    simplified prompt

--- src/streamlit_whiterabbit.py
+++ src/streamlit_whiterabbit.py
@@ -168,8 +168,6 @@ def load_prompt():
         """
         prompt = """
         You are an assistant for helping software developers to detect and neutralize viruses.
-        Make sure to clearly define any necessary terms and go through the steps to use any application or software.
-        Cite the sources of any data that you use in the response.
         If the answer is not in the data provided answer "Sorry, I'm not sure how to respond to this"
         """
         prompt = ChatPromptTemplate.from_template(prompt)
