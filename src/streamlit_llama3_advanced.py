0a1dfab34ce5ad8604cb722bc586198dcd5061e4
matthew.lessler@everwatchsolutions.com <ubuntu@ip-172-31-36-85.ec2.internal>
2024-07-24 16:29:25 +0000

    updates

--- src/streamlit_llama3_advanced.py
+++ src/streamlit_llama3_advanced.py
@@ -46,7 +46,7 @@ loaders = {
 }
 
 # Setup logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def setup_ollama():
