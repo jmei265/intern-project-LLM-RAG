0608b98329a72f4106f805a50ac6af167615b936
CMullikin-ews <connor.mullikin@everwatchsolutions.com>
2024-08-06 19:47:54 +0000

    update

--- src/streamlit_whiterabbit_redteam.py
+++ src/streamlit_whiterabbit_redteam.py
@@ -40,6 +40,7 @@ loaders = {
     '.pdf': PyPDFLoader
 }
 
+# Logger for catching error messages and listing docs used in each query
 logging.basicConfig(level=logging.INFO, filename = 'vector_log.log', filemode = 'w', format='%(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
